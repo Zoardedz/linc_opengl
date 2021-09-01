db39ba8bce9dbaf295e70186a86440d5aa6aeb26
Aidan63 <aidan.lee63@gmail.com>
2021-09-01 20:06:26 +0100

    Add hxparse dependency and array buffer view conversion for string pointer

--- opengl/StringPointer.hx
+++ opengl/StringPointer.hx
@@ -1,12 +1,22 @@
 package opengl;
 
 import haxe.io.Bytes;
+import haxe.io.ArrayBufferView;
 
 abstract StringPointer(cpp.Star<cpp.ConstCharStar>) from cpp.Star<cpp.ConstCharStar> to cpp.Star<cpp.ConstCharStar>
 {
     inline function new(_ptr)
     {
         this = _ptr;
+    }
+
+    @:from public static inline function fromArrayBufferView(_buffer : ArrayBufferView)
+    {
+        final src = cpp.NativeArray.address(_buffer.buffer.getData(), _buffer.byteOffset);
+        final ccs = (cast src.ptr : haxe.extern.AsVar<cpp.ConstCharStar>);
+        final ptr = cpp.Native.addressOf(ccs);
+
+        return new StringPointer(ptr);
     }
 
     @:from public static inline function fromBytes(_bytes : Bytes)
